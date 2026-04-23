2be6dc34e40708b09da85f76adf79681fafb6ac1
Average-Comrade <obychnyy.tovarishch@mail.ru>
2026-04-23 15:56:15 +0300

    ✨ (general): Add cycle to update elapsed items from redis stream to db

---
+++ elapsed_items.py
@@ -0,0 +1,27 @@
+from redis_queue import Consumer
+from redis import Redis
+from neo4j_birtix_db_repo.repos import ElapsedTimeRepository
+from neo4j_birtix_db_repo.models import ElapsedTimePayload
+from neo4j import GraphDatabase
+
+URI = "bolt://localhost:7690"
+AUTH = ("neo4j", "password")
+redis = Redis('localhost', 6378, decode_responses=True)
+driver = GraphDatabase.driver(URI, auth=AUTH)
+
+
+class ElapsedItemConsumer(Consumer):
+    def __init__(self, elapsed_items_repo: ElapsedTimeRepository, redis, stream_key, group_name, consumer, buffer_size, fulsh_time):
+        super().__init__(redis, stream_key, group_name, consumer, buffer_size, fulsh_time)
+        self.repo = elapsed_items_repo
+
+    def handle_batch(self, batch):
+        print(batch[0])
+        self.repo.upsert_batch([ElapsedTimePayload(**elapsed_items_tuple[1]) for elapsed_items_tuple in batch])
+
+
+elapsed_items_repo = ElapsedTimeRepository(driver)
+
+elapsed_items_consumer = ElapsedItemConsumer(elapsed_items_repo, redis, "elapsed_items", "group", "elapsed_items", 1000, 1)
+
+elapsed_items_consumer.run()
